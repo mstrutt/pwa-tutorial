aa34a9be8684fd111c6460d7cc588be49de8132d
Michael Strutt <michael@mstrutt.co.uk>
2019-08-04 17:21:30 +0100

    Offline page caching using SW Workbox

---
+++ app/js/sw-workbox.js
@@ -0,0 +1,6 @@
+// Using this file to house the generated list of files from parce-plugin-sw-cache
+var precacheFiles;
+function precacheAndRoute(files) {
+  precacheFiles = files;
+}
+precacheAndRoute([]);
--- app/service-worker.js
+++ app/service-worker.js
@@ -1,6 +1,16 @@
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute.mjs';
 import { MESSAGES, SYNCS } from "./js/config";
 import { updateContacts, postUnsyncedContacts } from './js/api-handlers';
 
+// Using SW Workbox to save all our files for offline access
+importScripts('./sw-workbox.js');
+if (precacheFiles) {
+  precacheAndRoute(precacheFiles);
+} else {
+  console.warn('Failed to precache files, offline functionality will be unavailable');
+}
+
+// Listening for the sync event
 self.addEventListener('sync', (event) => {
   console.debug('[SW] Sync triggered', event.tag);
   if (event.tag === SYNCS.UPDATE) {
@@ -24,11 +34,13 @@ self.addEventListener('sync', (event) => {
 
 self.skipWaiting();
 
+// Sequentially pushing up any unsynced changes, then pulling down latest data
 function syncContacts() {
   return postUnsyncedContacts()
     .then(() => updateContacts());
 }
 
+// Helper function we created to communicate with all connected browser windows
 function messageAllClients(message) {
   return clients.matchAll({ type: 'window' })
     .then((clientList) => {
